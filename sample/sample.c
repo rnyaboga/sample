bac4bb54f8685d0fe6a6dd61d0e9ec5766bdfa1c
rn <rnyaboga@gmail.com>
2018-06-18 15:45:37 -0700

    Initial Repo

--- sample/sample.c
+++ sample/sample.c
@@ -17,34 +17,34 @@
 //!
 //*****************************************************************************
 void writeBytesNonBlocking2(unsigned short nBytes) {
-	if ((memSize + nBytes) > PAGE_SIZE) {				//writes will exceed page
-		state = 0;										//goto to next page
+	if ((memSize + nBytes) > PAGE_SIZE) {               //writes will exceed page
+		state = 0;                                      //goto to next page
 	}
-	if ((memAddr + PAGE_SIZE) > (MEMSIZE)){				//last page of current die reached
-		if(uActiveChipsID + 1 > uNumOfChipsInstalled){	//last Die reached
+	if ((memAddr + PAGE_SIZE) > (MEMSIZE)){             //last page of current die reached
+		if(uActiveChipsID + 1 > uNumOfChipsInstalled){  //last Die reached
 			return;
 		}
-		uActiveChipsID++;								//move to next die
-		memAddr = 0;									//start page at zero
+		uActiveChipsID++;                               //move to next die
+		memAddr = 0;                                    //start page at zero
 		state = 1;
 	}
 	switch (state) {
-	case 0:												//close page
-		closeWriteBytesNonBlocking();					//close this page
-		memAddr += PAGE_SIZE;							//only increment when closed
+	case 0:                                             //close page
+		closeWriteBytesNonBlocking();                   //close this page
+		memAddr += PAGE_SIZE;                           //only increment when closed
 		memSize = 0;
 		state = 1;
-														//no break just roll to next case
-	case 1:												//prepate to write
+                                                        //no break just roll to next case
+	case 1:                                             //prepate to write
 		openWriteBytesNonBlocking();
 		state = 2;
-														//no break just roll to next case
-	case 2:												//write bytes
-		writeBytesCycle(nBytes);						//write bytes from buffer
+                                                        //no break just roll to next case
+	case 2:                                             //write bytes
+		writeBytesCycle(nBytes);                        //write bytes from buffer
 		memSize += nBytes;
 		if (memSize >= PAGE_SIZE) {
-			closeWriteBytesNonBlocking();				//close this page
-			memAddr += PAGE_SIZE;						//only increment when closed
+			closeWriteBytesNonBlocking();               //close this page
+			memAddr += PAGE_SIZE;                       //only increment when closed
 			memSize = 0;
 			state = 1;
 		}
